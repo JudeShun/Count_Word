6410826307cdad7c0e2d0e2fd39901b21c7856ef
cabalhaoma_sd2082@CEACCS.NET <cabalhaoma_sd2082@CEACCS.NET>
2019-09-25 16:01:03 +0800

    initial commit

---
+++ MJA_C_Util/src/StringManipulation/FilterHTML.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package StringManipulation;
+
+/**
+ *
+ * @author cabalhaoma_sd2082
+ */
+public class FilterHTML {
+    public String removeHTML(String str){
+        str=str.replaceAll("\\<.*?\\>","");
+        return str;
+    }
+    
+}
