bd8edc347ad021e2b243aad886380e85d18a77f4
cabalhaoma_sd2082@CEACCS.NET <cabalhaoma_sd2082@CEACCS.NET>
2019-10-16 17:08:49 +0800

    final wordcount

--- Filtering2/src/Filtering/NewJFrame.java
+++ Filtering2/src/Filtering/NewJFrame.java
@@ -12,12 +12,11 @@ import StringManipulation.FilterHTML;
  *
  * @author cabalhaoma_sd2082
  */
-public class NewjFrame extends javax.swing.JFrame {
-
-    /**
-     * Creates new form NewjFrame
+public class NewJFrame extends javax.swing.JFrame {
+/**
+     * Creates new form NewJFrame
      */
-    public NewjFrame() {
+    public NewJFrame() {
         initComponents();
     }
 
@@ -30,32 +29,32 @@ public class NewjFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jColorChooser1 = new javax.swing.JColorChooser();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        Output = new javax.swing.JTextArea();
         jScrollPane2 = new javax.swing.JScrollPane();
-        Original = new javax.swing.JTextArea();
-        jScrollPane3 = new javax.swing.JScrollPane();
-        Filtered = new javax.swing.JTextArea();
+        Input = new javax.swing.JTextArea();
         jButton1 = new javax.swing.JButton();
-
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        Original.setColumns(20);
-        Original.setRows(5);
-        jScrollPane2.setViewportView(Original);
+        Output.setColumns(20);
+        Output.setRows(5);
+        jScrollPane1.setViewportView(Output);
 
-        Filtered.setColumns(20);
-        Filtered.setRows(5);
-        jScrollPane3.setViewportView(Filtered);
+        Input.setColumns(20);
+        Input.setRows(5);
+        jScrollPane2.setViewportView(Input);
 
-        jButton1.setText("Remove HTML Tags");
+        jButton1.setText("CountWord");
         jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jButton1MouseClicked(evt);
+            }
+        });
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
             }
         });
 
@@ -66,38 +65,47 @@ public class NewjFrame extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(116, 116, 116)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(196, 196, 196)
+                        .addComponent(jButton1))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(137, 137, 137)
-                        .addComponent(jButton1)))
-                .addContainerGap(118, Short.MAX_VALUE))
+                        .addGap(67, 67, 67)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 354, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(96, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(64, 64, 64)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 353, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(100, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(238, Short.MAX_VALUE)
                 .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
-                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(36, 36, 36))
+                .addGap(30, 30, 30)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(34, 34, 34))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(43, 43, 43)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(282, Short.MAX_VALUE)))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
-       String str=Original.getText();
-       FilterHTML f=new FilterHTML();
-       str=f.removeHTML(str);
-       Filtered.setText(str);
-    }//GEN-LAST:event_jButton1MouseClicked
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
-    /**
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+       String str = Input.getText();
+       FilterHTML f = new FilterHTML();
+       str = f.removeHTML(str);
+       Output.setText(str);
+    }//GEN-LAST:event_jButton1MouseClicked
+ /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
@@ -114,31 +122,30 @@ public class NewjFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewjFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewjFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewjFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewjFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewjFrame().setVisible(true);
+                new NewJFrame().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextArea Filtered;
-    private javax.swing.JTextArea Original;
+    private javax.swing.JTextArea Input;
+    private javax.swing.JTextArea Output;
     private javax.swing.JButton jButton1;
+    private javax.swing.JColorChooser jColorChooser1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JScrollPane jScrollPane3;
-    private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
 }
--- MJA_C_Util/src/StringManipulation/FilterHTML.java
+++ MJA_C_Util/src/StringManipulation/FilterHTML.java
@@ -6,14 +6,115 @@
 
 package StringManipulation;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /**
  *
  * @author cabalhaoma_sd2082
  */
+//
+//REMOVE
+//        public class FilterHTML {
+//    public String removeHTML(String str){
+//        str=str.replaceAll("\\<.*?\\>","");
+//        return str;
+//    }
+//    
+//}
 public class FilterHTML {
-    public String removeHTML(String str){
-        str=str.replaceAll("\\<.*?\\>","");
-        return str;
-    }
     
+    
+    public String removeHTML(String str){
+         //GETTING THE TAGS OF THE WEBSITE
+        ArrayList<String>tags = new ArrayList<>();
+        //GETTING THE WORDS OF THE WEBSITE
+        ArrayList<String>words = new ArrayList<>(); 
+        //COUNTING THE WORD OF THE WEBSITE
+        ArrayList<String>wordcount = new ArrayList<>();
+       
+        String strs="";
+        
+  //GETTING THE TAGS OF THE WEBSITE
+        Pattern p = Pattern.compile("<([^\\s>/]+)");
+        Matcher m = p.matcher(str);
+        while (m.find()) 
+        {
+            String tag = m.group(1);
+            tags.add(tag);
+        }
+//-------------------------------------------------------------------------------------------------//
+        //GETTING THE WORDS OF THE WEBSITE
+        Pattern w = Pattern.compile("[a-zA-Z]+");  
+        Matcher m1 = w.matcher(str);
+
+        while (m1.find()) 
+        {
+            words.add(m1.group());
+        }
+        
+  //-------------------COUNT THE WORDS OF THE WEBSITE----------------------------//
+         //COUNTING THE WORD OF THE 
+           for (String word: words) 
+           {
+                  if(tags.contains(word)){
+                      continue;
+                  }
+                  else{
+                      wordcount.add(word);
+                  }
+             }
+ //----------THE OUTPUT OF HOW MANY TIMES THE WORD IN THE WEBSITE APPEARS---------//
+           Collections.sort(wordcount); 
+           Set<String> distinct = new HashSet<>(wordcount);
+        for (String s: distinct) 
+           {
+            strs+= s + ": " + Collections.frequency(wordcount,s)+"\n";
+     
+            try
+            {
+              // create a mysql database connection
+              String myDriver ="com.mysql.jdbc.Driver";
+              String myUrl = "jdbc:mysql://localhost/mydb";
+              Class.forName(myDriver);
+              Connection conn = DriverManager.getConnection(myUrl, "root", "");
+
+              // create a sql date object so we can use it in our INSERT statement
+              Calendar calendar = Calendar.getInstance();
+              java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+
+              // the mysql insert statement
+              String query = " insert into jude(words,count,school)"
+                + " values (?,?,?)";
+
+              // create the mysql insert preparedstatement
+              PreparedStatement preparedStmt = conn.prepareStatement(query);
+              preparedStmt.setString (1,s);
+              preparedStmt.setInt(2,Collections.frequency(wordcount,s));
+              preparedStmt.setString (3,"USC");
+
+
+              // execute the preparedstatement
+              preparedStmt.execute();
+
+              conn.close();
+            }
+            catch (Exception e)
+            {
+              System.err.println(e);
+              
+            }            
+   
+         }
+    return strs;  
+    }
 }
+
